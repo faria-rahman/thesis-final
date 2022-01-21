4bc1f025111995f82c09b2ccc3ac568728bd0893
faria-rahman <47113501+faria-rahman@users.noreply.github.com>
2022-01-21 20:10:24 +0600

    Create evaluation matrics.py

---
+++ evaluation matrics.py
@@ -0,0 +1,46 @@
+from keras.metrics import MeanIoU
+from sklearn.metrics import precision_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import roc_auc_score
+import keras.backend as K
+y_pred=model.predict(X_test)
+y_pred_argmax=np.argmax(y_pred, axis=3)
+score = f1_score(y_test, y_pred, average='binary')
+print('F-Measure: %.3f' % score)
+n_classes = 6
+IOU_keras = MeanIoU(num_classes=n_classes)  
+IOU_keras.update_state(y_test[:,:,:,0], y_pred_argmax)
+print("Mean IoU =", IOU_keras.result().numpy())
+values = np.array(IOU_keras.get_weights()).reshape(n_classes, n_classes)
+print(values)
+class1_IoU = values[0,0]/(values[0,0] + values[0,1] + values[0,2] + values[0,3] + values[1,0]+ values[2,0]+ values[3,0])
+class2_IoU = values[1,1]/(values[1,1] + values[1,0] + values[1,2] + values[1,3] + values[0,1]+ values[2,1]+ values[3,1])
+class3_IoU = values[2,2]/(values[2,2] + values[2,0] + values[2,1] + values[2,3] + values[0,2]+ values[1,2]+ values[3,2])
+class4_IoU = values[3,3]/(values[3,3] + values[3,0] + values[3,1] + values[3,2] + values[0,3]+ values[1,3]+ values[2,3])
+
+print("IoU for class1 is: ", class1_IoU)
+print("IoU for class2 is: ", class2_IoU)
+print("IoU for class3 is: ", class3_IoU)
+print("IoU for class4 is: ", class4_IoU)
+
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred = K.cast(y_pred, 'float32')
+    y_pred_f = K.cast(K.greater(K.flatten(y_pred), 0.5), 'float32')
+    intersection = y_true_f * y_pred_f
+    score = 2. * K.sum(intersection) / (K.sum(y_true_f) + K.sum(y_pred_f))
+    return score
+
+def dice_loss(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = y_true_f * y_pred_f
+    score = (2. * K.sum(intersection) + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return 1. - score
+
+def bce_dice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+
+def bce_logdice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) - K.log(1. - dice_loss(y_true, y_pred))
